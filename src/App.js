661d82c9ad7d93e7b1b94cc748b93e7950c1b3a4
vikussia48 <viktorijax4@gmail.com>
2022-10-10 15:59:05 +0100

    Created Home page and changed icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
-// import Home from './components/Home'
-// import About from './components/About'
-// import Contact from './components/Contact'
-// import Portfolio from './components/Portfolio'
-// import Dashboard from './components/Dashboard'
+import Home from './components/Home'
 import './App.scss'
 
 function App() {
@@ -12,11 +8,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          {/* <Route index element={<Home />} /> */}
-          {/* <Route path="about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/dashboard" element={<Dashboard />} /> */}
+          <Route index element={<Home />} />
         </Route>
       </Routes>
     </>
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,11 +1,15 @@
+import { Outlet } from 'react-router-dom'
 import Sidebar from '../Sidebar'
 import './index.scss'
 
 const Layout = () => {
   return (
-  <>
+  <div className='App'>
     <Sidebar />
-  </>
+    <div className='page'>
+      <Outlet />
+    </div>
+  </div>
   )
 }
 
