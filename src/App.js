2d7ff562d50004de00004489ac266f1b51f372da
vikussia48 <viktorijax4@gmail.com>
2022-10-27 11:43:36 +0100

    Solved broken parts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
-<<<<<<< Updated upstream
-// import Home from './components/Home'
-// import About from './components/About'
-// import Contact from './components/Contact'
 // import Portfolio from './components/Portfolio'
 // import Dashboard from './components/Dashboard'
-=======
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
->>>>>>> Stashed changes
 import './App.scss'
 
 function App() {
@@ -18,17 +12,11 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-<<<<<<< Updated upstream
-          {/* <Route index element={<Home />} /> */}
-          {/* <Route path="about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/dashboard" element={<Dashboard />} /> */}
-=======
           <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
->>>>>>> Stashed changes
+          {/* <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/dashboard" element={<Dashboard />} /> */}
         </Route>
       </Routes>
     </>
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,28 +1,10 @@
 import { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import Logo from '../../assets/images/VS_logo.png'
-<<<<<<< Updated upstream
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faLinkedin,
-  faGithub,
-  faYoutube,
-  faSkype,
-} from '@fortawesome/free-brands-svg-icons'
-import {
-  faHome,
-  faUser,
-  faEnvelope,
-  faSuitcase,
-  faBars,
-  faClose,
-} from '@fortawesome/free-solid-svg-icons'
-=======
 import { RiUser3Fill, RiHome2Fill } from 'react-icons/ri'
 import { SiGmail } from 'react-icons/si'
 import { BsLinkedin, BsGithub } from 'react-icons/bs'
 import './index.scss'
->>>>>>> Stashed changes
 
 const Sidebar = () => {
 
@@ -40,36 +22,21 @@ const Sidebar = () => {
         <NavLink 
           exact="true"
           activeclassname="active"
-<<<<<<< Updated upstream
-          to="/">
-          <FontAwesomeIcon icon={faHome} color="#B3B3B3" />
-=======
           to="/"
           onClick={() => setShowNav(false)}>
           <RiHome2Fill />
->>>>>>> Stashed changes
         </NavLink>
         <NavLink 
           activeclassname="active"
-<<<<<<< Updated upstream
-          to="/about">
-          <FontAwesomeIcon icon={faUser} color="#B3B3B3" />
-=======
           to="/about"
           onClick={() => setShowNav(false)}>
           <RiUser3Fill />
->>>>>>> Stashed changes
         </NavLink>
         <NavLink 
           activeclassname="active"
-<<<<<<< Updated upstream
-          to="/contact">
-          <FontAwesomeIcon icon={faEnvelope} color="#B3B3B3" />
-=======
           to="/contact"
           onClick={() => setShowNav(false)}>
           <SiGmail />
->>>>>>> Stashed changes
         </NavLink>
       </nav>
       <ul>
@@ -79,15 +46,7 @@ const Sidebar = () => {
             target="_blank"
             rel="noreferrer"
           >
-<<<<<<< Updated upstream
-            <FontAwesomeIcon
-              icon={faLinkedin}
-              color="#B3B3B3"
-              className="anchor-icon"
-            />
-=======
             <BsLinkedin />
->>>>>>> Stashed changes
           </a>
         </li>
         <li>
@@ -96,15 +55,7 @@ const Sidebar = () => {
             target="_blank"
             rel="noreferrer"
           >
-<<<<<<< Updated upstream
-            <FontAwesomeIcon
-              icon={faGithub}
-              color="#B3B3B3"
-              className="anchor-icon"
-            />
-=======
             <BsGithub />
->>>>>>> Stashed changes
           </a>
         </li>
       </ul>
